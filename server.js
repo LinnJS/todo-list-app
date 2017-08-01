61e5af47c9bfd4f3e39c7a50baf8b45ecc0467ac
JSLinn727 <justinslinn727@gmail.com>
2017-08-01 15:23:23 -0400

    Completed filejson implementation and made routes for auth page.

--- server.js
+++ server.js
@@ -3,23 +3,44 @@ const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
 const jsonfile = require('jsonfile')
 const expressSession = require('express-session')
-const expressVal = require('express-validator')
+const expressValidator = require('express-validator')
+
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(expressValidator())
 app.use(express.static('public'))
+app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}))
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
-const todoList = []
-const completedList = []
+const todoList = jsonfile.readFileSync('todos.json', {throws:false}) || []
+
+app.get("/login", (req, res) =>{
+  // display the form to login
+})
+app.post("/login"(req,res) =>{
+  // set session
+  //redirect to /
+})
+const checkAuthenication = (req, res,next) => {
+ if (req.session.username){
+   next()
+ } else{
+   res.redirect("/login")
+ }
+}
+
+app.use(checkAuthenication)
 
 app.get('/', (req, res) => {
   const templateData = {
     uncompleted: todoList.filter(todo => !todo.completed),
-    completed: completedList
+    completed: todoList.filter(todo => todo.completed),
+    anyItem: todoList.length > 0
   }
 
   res.render('home', templateData)
@@ -28,17 +49,42 @@ app.get('/', (req, res) => {
 app.post('/addTodo', (req, res) => {
   const newTodoDescript = req.body.description
 
-  todoList.push(newTodoDescript)
+  //todoList.push(newTodoDescript)
+  todoList.push({
+    id: todoList.length + 1,
+    completed: false,
+    description: newTodoDescript
+  })
+  console.log(todoList)
+
+  jsonfile.writeFile('todos.json', todoList, {
+    spaces: 2
+  }, err => {
+    console.log(`todos.json error ${err}`)
+  })
+
   res.redirect('/')
 })
 
 app.post('/markComplete', (req, res) => {
-  const description = req.body.description
-  completedList.push(description)
+  //const description = req.body.description
+  const id = parseInt(req.body.id)
 
-  const indexOfItem = todoList.indexOf(description)
+  //  completedList.push(description)
+  const todo = todoList.find(todo => todo.id === id)
 
-  todoList.splice(indexOfItem, 1)
+  // const indexOfItem = todoList.indexOf(description)
+  // todoList.splice(indexOfItem, 1)
+  if (todo) {
+    todo.completed = true
+  }
+
+  jsonfile.writeFile('todos.json', todoList, {
+    spaces: 2
+  }, err => {
+    console.log(`todos.json error: ${err}`)
+  })
+
   res.redirect('/')
 })
 
