597cb8c5c585d71448d9cda5096acfd7d8d6b851
JSLinn727 <justinslinn727@gmail.com>
2017-09-16 11:55:37 -0400

    Commented out login auth

--- server.js
+++ server.js
@@ -7,50 +7,50 @@ const expressValidator = require('express-validator')
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(expressValidator())
 app.use(express.static('public'))
-app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}))
+app.use(expressSession({ secret: 'max', saveUninitialized: false, resave: false }))
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
-app.get('/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/login', (req, res, err) => {
-
-  res.redirect('/')
-})
-
-app.post('/loginSubmit', (req, res, err) => {
-  const auth = {
-    "email": "justin@gmail.com",
-    "password": "password"
-  }
-
-  const input = {
-    "email": req.body.email,
-    "password": req.body.password
-  }
-  req.session.isLoggedIn = req.body.email === auth.email && req.body.password === auth.password
-  if (req.session.isLoggedIn) {
-    res.redirect('/')
-  } else {
-    res.render('login')
-  }
-})
-
-const checkAuthenication = (req, res, next) => {
-  if (req.session.isLoggedIn) {
-    next()
-  } else {
-    res.redirect('login')
-  }
-}
-app.use(checkAuthenication)
+// app.get('/login', (req, res) => {
+//   res.render('login')
+// })
+//
+// app.post('/login', (req, res, err) => {
+//
+//   res.redirect('/')
+// })
+//
+// app.post('/loginSubmit', (req, res, err) => {
+//   const auth = {
+//     "email": "justin@gmail.com",
+//     "password": "password"
+//   }
+//
+//   const input = {
+//     "email": req.body.email,
+//     "password": req.body.password
+//   }
+//   req.session.isLoggedIn = req.body.email === auth.email && req.body.password === auth.password
+//   if (req.session.isLoggedIn) {
+//     res.redirect('/')
+//   } else {
+//     res.render('login')
+//   }
+// })
+//
+// const checkAuthenication = (req, res, next) => {
+//   if (req.session.isLoggedIn) {
+//     next()
+//   } else {
+//     res.redirect('login')
+//   }
+// }
+// app.use(checkAuthenication)
 
 app.get('/', (req, res) => {
   const todoList = req.session.todoList || []
